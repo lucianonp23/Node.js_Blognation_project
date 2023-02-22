fb491f6334badcb7b9255aac4506f09769032b72
Luciano Pena <lucianonp23@gmail.com>
2023-02-22 19:35:42 +0000

    implemented hash password with bcrypt

--- users/usersController.js
+++ users/usersController.js
@@ -1,9 +1,29 @@
 const express= require("express");
 const router= express.Router();
 const Users= require ("./Users");
+const bcrypt= require("bcryptjs");
 
 router.get("/admin/users", (req,res)=>{
-    res.send("listagem de usuarios");
+    res.render("./admin/users/create");
+});
+
+router.post("/admin/users/create", (req,res)=>{
+    var email= req.body.email;
+    var password= req.body.password; 
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash= bcrypt.hashSync(password,salt);
+
+    if(hash !=undefined){
+       Users.create({
+        email:email,
+        password:hash
+       }).then(()=>{
+        res.redirect("/admin/users");
+       }) 
+    }
+    
+    
 })
 
 
