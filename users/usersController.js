8b72e2fe5f46dc00ab4caa875d5aaf2aee780362
Luciano Pena <lucianonp23@gmail.com>
2023-02-25 10:47:37 +0000

    Implemented Middleware for user authentication

--- users/usersController.js
+++ users/usersController.js
@@ -2,10 +2,11 @@ const express= require("express");
 const router= express.Router();
 const Users= require ("./Users");
 const bcrypt= require("bcryptjs");
+const adminAuth=require("../middlewares/adminAuth");
 
 
 //page for list of users
-router.get("/admin/users",(req,res)=>{
+router.get("/admin/users",adminAuth,(req,res)=>{
     Users.findAll().then(users=>{
        res.render("admin/users/index",{users:users}) 
     })
@@ -41,9 +42,38 @@ router.post("/users/create", (req,res)=>{
         }
     });
     
-    
-    
-    
+
+});
+
+//login authentication
+router.get("/users/login",(req,res)=>{
+    res.render("admin/users/login");
+});
+
+router.post("/users/authenticate",(req,res)=>{
+    var email= req.body.email;
+    var password = req.body.password;
+
+    Users.findOne({where:{
+        email:email
+    }}).then(user=>{
+        if(user !=undefined){
+            var passwordCheck= bcrypt.compareSync(password,user.password)
+            if (passwordCheck){
+                req.session.user={
+                    email:email,
+                    password:password
+                }
+            res.json(req.session.user);
+            }else {
+                res.redirect("/users/login");   
+            }
+            
+           
+        }else{
+            res.redirect("/users/login");
+        }
+    })
 })
 
 
